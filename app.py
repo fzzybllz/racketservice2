d629d2ec7fd8ef0b240aeeb40068fe795457dd87
fzzybllz <fzzybllz@gmail.com>
2021-11-01 00:32:22 +0100

    Add config support

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
+env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
+app.config.from_object(env_config)
 
 @app.route("/")
 def index():
-    return "This is the last staging test version!"
+    secret_key = app.config.get("SECRET_KEY")
+    return f"The configured secret key is {secret_key}."
