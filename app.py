ef89b80298919edef6e0b8eee2eebe3f4e86efb2
fzzybllz <53255868+fzzybllz@users.noreply.github.com>
2022-08-06 22:10:48 +0200

    first form

--- app.py
+++ app.py
@@ -1,17 +1,51 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import (
+    DataRequired,
+    Email,
+    EqualTo,
+    Length,
+    Optional
+)
 
 # Create a Flask Instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'my secret key'
+
+# Create Form Class
+class SignupForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[Length(min=6), Email(message='Keine gültige E-Mail'), DataRequired()])
+    password = PasswordField('Passwort', validators=[DataRequired(), Length(min=8, message='Passwort zu kurz')])
+    confirm = PasswordField('Bestätige dein Passwort', validators=[DataRequired(), EqualTo('password', message='Passwörter stimmen nicht überein')])
+    submit = SubmitField('Registrieren')
 
 # Route to Home
 @app.route("/")
 def home():
-    return render_template("home.jinja2")
+    return render_template("home.html")
+
+@app.route("/signup", methods=['Get', 'POST'])
+def signup():
+    name = None
+    email = None
+    form = SignupForm()
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template("signup.html",
+        name = name,
+        email = email,
+        password = password,
+        confirm = confirm,
+        form = form)
 
 # Error Page
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template("404.jinja2"), 404
+    return render_template("404.html"), 404
 @app.errorhandler(500)
 def page_not_found(e):
-    return render_template("500.jinja2"), 500
+    return render_template("500.html"), 500
