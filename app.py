53bb5f5482b5d9c6d9841194cc1080f28e854e7e
fzzybllz <53255868+fzzybllz@users.noreply.github.com>
2022-08-07 13:54:26 +0200

    login form added

--- app.py
+++ app.py
@@ -5,40 +5,61 @@ from wtforms.validators import (
     DataRequired,
     Email,
     EqualTo,
-    Length,
-    Optional
+    Length
 )
 
 # Create a Flask Instance
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'my secret key'
 
-# Create Form Class
+# Create Form Classes
 class SignupForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
+    firstname = StringField('Vorname', validators=[DataRequired()])
+    lastname = StringField('Name', validators=[DataRequired()])
     email = StringField('Email', validators=[Length(min=6), Email(message='Keine gültige E-Mail'), DataRequired()])
     password = PasswordField('Passwort', validators=[DataRequired(), Length(min=8, message='Passwort zu kurz')])
     confirm = PasswordField('Bestätige dein Passwort', validators=[DataRequired(), EqualTo('password', message='Passwörter stimmen nicht überein')])
     submit = SubmitField('Registrieren')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Passwort', validators=[DataRequired()])
+    submit = SubmitField('Login')
 
 # Route to Home
 @app.route("/")
 def home():
     return render_template("home.html")
 
+# Route to Signup
 @app.route("/signup", methods=['Get', 'POST'])
 def signup():
-    name = email = password = confirm = None
+    firstname = lastname = email = password = confirm = None
     form = SignupForm()
     # Validate Form
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
+        email = form.email.data
+        form.email.data = ''
     return render_template("signup.html",
-        name = name,
+        firstname = firstname,
+        lastname = lastname,
         email = email,
         password = password,
         confirm = confirm,
+        form = form)
+
+# Route to Login
+@app.route("/login", methods=['Get', 'POST'])
+def login():
+    email = password = None
+    form = LoginForm()
+    # Validate Form
+    if form.validate_on_submit():
+        email = form.email.data
+        form.email.data = ''
+    return render_template("login.html",
+        email = email,
+        password = password,
         form = form)
 
 # Error Page
